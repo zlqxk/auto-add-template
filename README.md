7c9c5485a24db0f59f82a727c2523009056e4287
zhanglu <809740443@qq.com>
2020-09-21 01:49:58 +0800

    feat

---
+++ README.md
@@ -0,0 +1,4 @@
+### 主要实现---》 ./autoAdd_script
+
+1、使用npm run dev来运行脚本
+2、运行后，在components目录下添加文件将会被自动引入到index.tsx中
---
+++ src/components/ReactChild/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ReactChild:React.FC = props => {
+  return (
+    <div>
+      ReactChild1111
+    </div>
+  )
+}
+
+export default ReactChild
